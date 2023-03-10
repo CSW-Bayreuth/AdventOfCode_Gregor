3d54e24c325365b2bab9f583b3061bdebb1895e9
culorin <culorin@users.noreply.github.com>
2023-03-10 01:41:11 +0100

    + Day 5.1 Read CraneCmd

--- aoc_22_05/src/lib.rs
+++ aoc_22_05/src/lib.rs
@@ -4,7 +4,7 @@
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::path::Path;
+// use std::path::Path;
 
 // ----------------------------------------------------
 // types + traits
@@ -38,20 +38,46 @@ pub fn start_app() {
 
 impl Reader<Cargo> for String {
     fn read(&self) -> Option<Cargo> {
-        None
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"\[([[:alpha:]])\]").unwrap();
+        }
+        const ERR_MSG_MATCH: &str = "cargo input match did not unwrap";
+        const ERR_MSG_ALPHA: &str = "cargo should have format '[a-zA-Z]'";
+
+        RE.captures_iter(self)
+            .next()
+            .expect(ERR_MSG_MATCH)
+            .iter()
+            .next()
+            .expect(ERR_MSG_MATCH)
+            .expect(ERR_MSG_ALPHA)
+            .as_str()
+            .chars()
+            .next()
     }
 }
 
 impl Reader<CraneCmd> for String {
     fn read(&self) -> Option<CraneCmd> {
         lazy_static! {
-            static ref RE: Regex = Regex::new(r"move (\d) from (\d to (\d)").unwrap();
+            static ref RE: Regex = Regex::new(r"move (\d) from (\d) to (\d)").unwrap();
         }
-        const ERR_MSG: &str = "crane cmd should have format 'move UINT from UINT to UINT'";
+        const ERR_MSG_MATCH: &str = "crane cmd input match did not unwrap";
+        const ERR_MSG_UINT: &str = "crane cmd should have format 'move UINT from UINT to UINT'";
 
-        RE.captures(self)?
-            .iter()
-            .map(|c| c.expect(ERR_MSG).as_str().parse::<usize>().expect(ERR_MSG))
-            .next_tuple()
+        RE.captures_iter(self)
+            .map(|m| {
+                m.iter()
+                    .skip(1)
+                    .map(|c| {
+                        c.expect(ERR_MSG_MATCH)
+                            .as_str()
+                            .parse::<usize>()
+                            .expect(ERR_MSG_UINT)
+                    })
+                    .next_tuple::<CraneCmd>()
+            })
+            .next()
+            .unwrap()
     }
 }
--- aoc_22_05/tests/test.rs
+++ aoc_22_05/tests/test.rs
@@ -1,20 +1,25 @@
 // ----------------------------------------------------
 // imports
 // ----------------------------------------------------
-use std::path::Path;
+// use std::path::Path;
 
 use aoc_22_05::*;
 
 // ----------------------------------------------------
 // tests constants
 // ----------------------------------------------------
-const INPUT_PROPER_PATH: &str = "./../input/aoc_22_05/input.txt";
-const INPUT_SAMPLE_PATH: &str = "./../input/aoc_22_05/input_example.txt";
+// const INPUT_PROPER_PATH: &str = "./../input/aoc_22_05/input.txt";
+// const INPUT_SAMPLE_PATH: &str = "./../input/aoc_22_05/input_example.txt";
 
 // ----------------------------------------------------
 // test funcs part 1
 // ----------------------------------------------------
 #[test]
-fn test() {
-    assert!(true);
+fn test_read_cargo() {
+    assert_eq!(String::from("[A]").read(), Some('A'));
+}
+
+#[test]
+fn test_read_crane_cmd() {
+    assert_eq!(String::from("move 1 from 2 to 1").read(), Some((1, 2, 1)));
 }
